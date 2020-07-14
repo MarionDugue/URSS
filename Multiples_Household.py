f46782342403e9fe76c9ae3ac5a459edc1206f8c
MarionDugué <67784742+MarionDugue@users.noreply.github.com>
2020-07-14 20:16:07 +0100

    Update Multiples_Household.py

--- Multiples_Household.py
+++ Multiples_Household.py
@@ -1,8 +1,9 @@
+
 import scipy.stats as stats
 from collections import Counter
 
 #---------------------------------PARAMETERS--------------------
-population_size = 1000
+population_size = 100
 
 #--------------------------------DEFINING INITIAL HOUSEHOLD LIST----
 Household_List = []
@@ -32,7 +33,6 @@ Household_List = Household_List[:population_size]
 #--Counts each value in Household_List and creates a dictionary---------------------
 
 Household_Dict = dict(Counter(Household_List))
-print(Household_Dict)
 
 
 #--Checking for multiples--------------------------------------------  
@@ -51,19 +51,25 @@ for size in Household_Dict:
             else:
                 Not_sensible[size] = Household_Dict[size]
         else:
-            if Household_Dict[size] % size == 0:
-                b = (size, "is sensible")
-                Checking_multiples.append(b)
-                Sensible[size] = Household_Dict[size]
-            else:
+            if Household_Dict[size] == 1:
                 Not_sensible[size] = Household_Dict[size]
+            else:
+                if Household_Dict[size] % (size+1) == 0:
+                    b = (size, "is sensible")
+                    Checking_multiples.append(b)
+                    Sensible[size] = Household_Dict[size]
+                else:
+                    Not_sensible[size] = Household_Dict[size]
 
 #--Sorting values of Not_sensible & Sensible dictionary-------------------------------------            
 SNot_List = sorted(Not_sensible.items(), key=lambda x: x[0], reverse=True)
-#print("TO TWEAK", SNot_List)
 #Take the 1st not sensible key
 Sorted_Household =  sorted(Household_Dict.items(), key=lambda x: x[0])
 
+# SNot_List = [(4, 2), (2, 2), (1, 3)]
+# print("TO TWEAK", SNot_List)
+# Sorted_Household = [(0, 3), (1, 3), (2, 2), (4, 2)]
+# print("SORTED IS ", Sorted_Household)
 
 #--If pair is not sensible, its count will be carried out to the next one-----------------
 credit = 0
@@ -71,10 +77,11 @@ newpairs = []
 
 for pair in SNot_List:
     key = pair[0]
-    remainder = 0
+    
     if credit != 0:
         value = pair[1] + credit 
         credit = 0
+        
     else:
         value = pair[1]
     if key == 1:
@@ -84,34 +91,45 @@ for pair in SNot_List:
                 Value_0 = Sorted_Household[0][1] + 1
                 newpairs.append((Sorted_Household[0][0], Value_0))
                 newpairs.append((Sorted_Household[1][0], value))
-      
             else:
                 newpairs.append((key, value))
     else:
-        if value % key != 0:
-            #for the pair with the largest value, deduce value until satisfy divisibility condition
-            while value % key != 0:
-                value = value-1
-                credit += 1 
-                i += 1
+        if value == 1:
+                value = 0
+                credit += 1
+                newpairs.append((key, value))
+        if value == 0:
             newpairs.append((key, value))
-            if SNot_List[-1] == pair:
-                for pair in Sorted_Household:
-                    if pair[0] == 1:
-                        value = pair[1] + credit
-                        if value % 2 != 0:
-                            value = value - 1
-                            Value_0 = Sorted_Household[0][1] + 1
-                            newpairs.append((Sorted_Household[1][0], value))
-                            newpairs.append((Sorted_Household[0][0], Value_0))
-                        else:
-                            newpairs.append((Sorted_Household[1][0], value))
-                
         else:
-            newpairs.append((key, value))
-           
+            if value % (key+1) != 0:
+                #for the pair with the largest value, deduce value until satisfy divisibility condition
+                while value % (key+1) != 0:
+                    value = value - 1
+                    credit += 1
+                if value == 1:
+                     value = 0
+                     credit += 1
+                newpairs.append((key, value))
+                if SNot_List[-1] == pair:
+                    for pair in Sorted_Household:
+                        if pair[0] == 1:
+                            value = pair[1] + credit
+                            if value % 2 != 0:
+                                value = value - 1
+                                Value_0 = Sorted_Household[0][1] + 1
+                                newpairs.append((Sorted_Household[1][0], value))
+                                newpairs.append((Sorted_Household[0][0], Value_0))
+                            else:
+                                newpairs.append((Sorted_Household[1][0], value))
+                
+            else:
+                newpairs.append((key, value))
+
+
+
 
 #--Appending sensible pairs to the new list------------------------------------    
+
 i = 0
 while i < len(Sorted_Household):
     a = Sorted_Household[i][0]
@@ -125,16 +143,20 @@ while i < len(Sorted_Household):
     i += 1
 
 #--Removing all pairs with a count = 0-----------------------------------------
-for pair in newpairs:
-    if pair[1] == 0:
-        newpairs.remove(pair)
 
-#--Transforming List of pairs into list of integers---------------------------           
+for n in newpairs:
+    if n[1] == 0:
+        newpairs.remove(n)
+    
+
+#--Transforming List of pairs into list of integers---------------------------      
+
 New_Householdlist = []
 for pair in newpairs:
     i = 0
     while i < pair[1]:
         New_Householdlist.append(pair[0])
         i += 1
-print(newpairs)
 
+
+print(newpairs)
